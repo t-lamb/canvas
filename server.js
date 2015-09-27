1d7eae1c7bd632beb7696203b30229e5f9093e0d
Teresa Lamb <flour1337@gmail.com>
2015-09-27 17:01:42 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+var http = require('http');
+var fs = require('fs'); //file system
+var url = require('url');
+
+function requestHandler(req, res) {
+	//console.log("The URL is: " + req.url);
+
+	var parsedUrl = url.parse(req.url);
+	//console.log("They asked for " + parsedUrl.pathname);
+
+	var path = parsedUrl.pathname;
+	if (path == "/") {
+		path = "index.html";
+		console.log("index!")
+	}
+
+	// read index.html
+	fs.readFile(__dirname + '/' + path, 
+		// callback function for reading
+
+		function (err, fileContents) {
+			// error handling
+			//console.log(__dirname + '/' + path);
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + req.url);
+			}
+			res.writeHead(200);
+			res.end(fileContents);	
+		}
+	);
+	console.log("Got a request " + req.url);
+}
+
+var httpServer = http.createServer(requestHandler);
+httpServer.listen(8081);
+console.log("Server listening on port 8081");
+
+// WebSocket Portion
+// WebSockets work with the HTTP httpServer
+var io = require('socket.io').listen(httpServer);
+
+// Register a callback function to run when we have an individual connection
+// runs for each user that connects
+io.sockets.on('connection',
+	// We are given a web socket object in our function
+	function (socket) {
+		console.log("We have a new client: " + socket.id);
+
+		// when this user emits, client side: socket.emit('otherevent', some data);
+		socket.on('othermessage', function(data){
+			// data comes in when it is sent, including objects
+			console.log("Received: 'othermouse' " + data.x + " " + data.y);
+			// send it to all the clients including self
+			socket.emit('othermouse', data);
+		});
+
+		socket.on('disconnect', function(){
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
